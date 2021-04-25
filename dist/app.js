c9efa8ca4748fea3f44ea89151b685e8b8ff0acc
tyogautomo <yoga.utomo27@gmail.com>
2021-04-26 05:39:33 +0700

    asd

--- dist/app.js
+++ dist/app.js
@@ -1,104 +1,41 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
+let person1;
+person1 = {
+    name: 'Tony',
+    // age: 12, // this will error, object literal must have the exact same property as interface
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}`);
+    },
+    walk: () => { }
+};
+person1.greet('Hello my name is');
+class Person {
+    constructor(name, age, sex, gender) {
         this.name = name;
-        // name: string; // you dont need specify public modifier bcs its default
-        this.employees = [];
+        this.age = age;
+        this.sex = sex;
+        this.gender = gender;
+        this.cardId = '12312';
     }
-    ; // if you give a modifier to parameter, it becomes a property
-    addEmployee(employee) {
-        // console.log(Department.fiscalYear); // if you want access static property on class method, access from the class instead
-        this.employees.push(employee);
+    ; // age will not error
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}`);
     }
-    ;
-    printEmployeesInfo() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-    ;
-    static createEmployee(name) {
-        // console.log(this.fiscalYear, 'from create employee'); // if you're accessing static property from static method, you can use 'this' directly
-        return { name };
+    changeCardId(id) {
+        this.cardId = id;
     }
 }
-Department.fiscalYear = 2021;
 ;
-class IT extends Department {
-    constructor(id, admins) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-    describe() {
-        console.log(`IT Departement - ID: ${this.id}`);
+class Abnormal extends Person {
+    walk() {
+        console.log('Im walking haha');
+        return 'walking'; // can return something even we abstract it as void
     }
 }
-class Accounting extends Department {
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-        this._lastReport = reports[0];
-    }
-    get lastReport() {
-        if (this._lastReport) {
-            return this._lastReport;
-        }
-        throw new Error('404 last report not found.');
-    }
-    set lastReport(report) {
-        if (!report) {
-            throw new Error('Please input a valid value');
-        }
-        this.addReport(report);
-    }
-    static createInstance() {
-        if (this.instance) {
-            return this.instance;
-        }
-        this.instance = new Accounting('d2', []);
-        return this.instance;
-    }
-    describe() {
-        console.log(`Accounting Department - ID: ${this.id}`);
-    }
-    addEmployee(employee) {
-        if (employee === 'Max') {
-            return;
-        }
-        this.employees.push(employee);
-    }
-    addReport(report) {
-        this.reports.push(report);
-        this._lastReport = report;
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-}
-const it = new IT('d1', ['Max']);
-// const accounting = new Accounting('d2', []);
-const accounting = Accounting.createInstance();
-const accounting2 = Accounting.createInstance();
-const employee = Department.createEmployee('Toni');
-console.log(employee, ' ', Department.fiscalYear);
-const itCopy = { describe: it.describe };
-it.describe();
-accounting.describe();
-// itCopy.describe(); // this will error compile bcs this not refer to Department, 
-// except we pass 'this' as parameter on describe() and add 'name' key on itCopy object
-it.addEmployee('Dono');
-it.addEmployee('Kasino');
-// it.employees[2] = 'Dadang'; // this will error compile bcs employees is a private property
-it.printEmployeesInfo();
-console.log(it);
-console.log(accounting);
-console.log(accounting === accounting2, 'same ???');
-accounting.addReport('money lose');
-accounting.addEmployee('Max');
-accounting.addEmployee('Jondo');
-accounting.printReports();
-accounting.printEmployeesInfo();
-console.log(accounting.lastReport);
-accounting.lastReport = 'taeguki';
-console.log(accounting.lastReport);
-console.log(accounting2);
+let person2;
+person2 = new Abnormal('Mikael', 24, 'm', 's');
+console.log(person2);
+person2.gender; // If its type refer to 'Biological', it only have 2 property
+console.log(person2.cardId);
+person2.changeCardId('311');
+console.log(person2.cardId);
