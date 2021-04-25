b768ccfe267562494d050fdaff5174b01fc874d8
tyogautomo <yoga.utomo27@gmail.com>
2021-04-26 06:52:35 +0700

    type guard

--- src/app.ts
+++ src/app.ts
@@ -16,14 +16,52 @@ const tesserac: Force = {
   skills: ['absorbing']
 }
 
+const crystalRed: Force = {
+  name: 'Red Crystal',
+  skills: ['venom']
+}
+
 function printForceInfo(force: Force) {
   console.log(force.name);
-  if ('startDate' in force) {
+  if ('startDate' in force) { // type guarding
     console.log(force.startDate);
   }
-  if ('skills' in force) {
+  if ('skills' in force) { // type guarding
     console.log(force.skills);
   }
+  console.log('//===========================//')
 };
 
 printForceInfo(tesserac);
+printForceInfo(crystalRed);
+
+//============================ Class Case ===============================
+
+class Car {
+  drive() {
+    console.log('driving a car...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('driving a truck...');
+  }
+
+  loadPackage() {
+    console.log('loading a package...');
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const useVehicle = (vehicle: Vehicle) => {
+  if (vehicle instanceof Truck) { // kind of type guarding
+    vehicle.loadPackage(); // if guarding not given, this loadPackage method will not exist
+  }
+  vehicle.drive();
+  console.log('//=============================//');
+};
+
+useVehicle(new Car());
+useVehicle(new Truck());
