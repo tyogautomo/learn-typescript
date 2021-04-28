35169f7bb80a70ec030cf66837db515ece26b6b3
tyogautomo <yoga.utomo27@gmail.com>
2021-04-29 03:53:31 +0700

    asd

--- src/app.ts
+++ src/app.ts
@@ -25,4 +25,20 @@ const objB = {
 
 const mergedObj = merge(objA, objB);
 
-mergedObj.something;
+mergedObj.something; // they know if the return value have 'something' key;
+
+type Lengthy = {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptor = 'We got no value.';
+  if (element.length === 1) {
+    descriptor = 'We got 1 value';
+  } else if (element.length > 1) {
+    descriptor = `We got ${element.length} value`;
+  }
+  return [element, descriptor];
+};
+
+console.log(countAndDescribe('whattheheck'));
