b0f8d5c104e42707757565748e28271196da5d74
tyogautomo <yoga.utomo27@gmail.com>
2021-05-02 02:36:53 +0700

    decort

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
-const Logger: Function = (constructor: Function): void => {
-  console.log('loggin on Mutant class...');
+const Logger: Function = (logString: string): Function => (constructor: Function): void => {
+  console.log(logString);
+  console.log(constructor, ' <<<< constructor');
 }
 
-@Logger
+const WithTemplate = (template: string, htmlId: string) => (constructor: any) => {
+  const element: HTMLElement = <HTMLDivElement>document.getElementById(htmlId);
+  const construct = new constructor();
+  if (element) {
+    element.innerHTML = template; // inserting a template to that element
+    element.querySelector('h1')!.textContent = construct.name; // asume that we always have h1 element inside that element
+  }
+}
+
+// @Logger('LOGGING - MUTANT') // <== decorator: must return a function
+@WithTemplate('<h1>This is Title from Decorator</h1>', 'title') // <== decorator: must return a function
 class Mutant {
   name = "Tonol";
 
   constructor() {
-    console.log('Creating an instance...');
+    console.log('Creating an instance neh..');
   }
 }
 
