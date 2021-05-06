a697b7f925f781c50c5fb16101d155b3468a4ca2
tyogautomo <yoga.utomo27@gmail.com>
2021-05-07 03:11:28 +0700

    asd

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,51 @@
-function Require() { };
+interface ValidatorConfig {
+  [prop: string]: {
+    [validateableProp: string]: string[]
+  }
+};
 
-function PositiveNumber() { };
+const registeredValidators: ValidatorConfig = {
+  // Course: {
+  //   title: ['required'],
+  //   price: ['positive']
+  // }
+};
 
-function validate(obj: object): boolean {
-  return false;
+function Require(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required']
+  }
+};
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive']
+  }
+};
+
+function validate(obj: any): boolean {
+  const classValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!classValidatorConfig) {
+    return true;
+  }
+  let check = true;
+  for (const prop in classValidatorConfig) {
+    for (const validator of classValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          check = check && !!obj[prop];
+          break;
+        case 'positive':
+          check = check && obj[prop] > 0;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return check;
 }
 
 class Course {
@@ -31,8 +73,9 @@ form.addEventListener('submit', (e) => {
   const course = new Course(title, price);
 
   if (!validate(course)) {
-    throw new Error('Validation failed');
+    alert('Invalid input, please try again!');
+    return;
   }
 
-  console.log(course);
+  console.log(course, '<<<<<<<<<<<<<<');
 })
