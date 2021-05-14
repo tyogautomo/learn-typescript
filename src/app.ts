5659c6eb7c7411ba0bd37a31551e7bced618f8c0
tyogautomo <yoga.utomo27@gmail.com>
2021-05-14 20:29:15 +0700

    asd

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,3 @@
-// validation helper
-interface Validatable {
-  value: string | number;
-  required?: boolean;
-  maxLength?: number;
-  minLength?: number;
-  max?: number;
-  min?: number;
-}
-
-const validator = (input: Validatable): boolean => {
-  let isValid = true;
-  if (input.required) {
-    isValid = isValid && (input.value.toString().trim().length > 0);
-  }
-  if (typeof input.value === 'string') {
-    if (input.maxLength) {
-      isValid = isValid && (input.value.length <= input.maxLength);
-    }
-    if (input.minLength) {
-      isValid = isValid && (input.value.length >= input.minLength);
-    }
-  }
-  if (typeof input.value === 'number') {
-    if (input.max) {
-      isValid = isValid && (input.value <= input.max);
-    }
-    if (input.min) {
-      isValid = isValid && (input.value >= input.min);
-    }
-  }
-  return isValid;
-}
-
-// decorator
-const autobind = (_: any, __: string, descriptor: PropertyDescriptor) => {
-  const method = descriptor.value; // get the original method function
-  const newDescriptor: PropertyDescriptor = {
-    get() {
-      return method.bind(this); // bind the method to this
-    }
-  };
-  return newDescriptor;
-}
-
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -86,10 +41,29 @@ class ProjectInput {
     const description = this.descriptionInputElement.value;
     const people = this.peopleInputElement.value;
 
+    const titleValidator: Validatable = {
+      value: title,
+      required: true,
+      minLength: 1,
+      maxLength: 20
+    }
+    const descValidator: Validatable = {
+      value: description,
+      required: true,
+      minLength: 5,
+      maxLength: 20
+    }
+    const peopleValidator: Validatable = {
+      value: +people,
+      required: true,
+      max: 20,
+      min: 1
+    }
+
     if (
-      !title.trim() ||
-      !description.trim() ||
-      !people.trim()
+      !validator(titleValidator) ||
+      !validator(descValidator) ||
+      !validator(peopleValidator)
     ) {
       return;
     }
@@ -109,6 +83,51 @@ class ProjectInput {
   private attachElement() {
     this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
   }
+}
+
+// validation helper
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  maxLength?: number;
+  minLength?: number;
+  max?: number;
+  min?: number;
+}
+
+// decorator
+function autobind(_: any, __: string, descriptor: PropertyDescriptor) {
+  const method = descriptor.value; // get the original method function
+  const newDescriptor: PropertyDescriptor = {
+    get() {
+      return method.bind(this); // bind the method to this
+    }
+  };
+  return newDescriptor;
+}
+
+const validator = (input: Validatable): boolean => {
+  let isValid = true;
+  if (input.required) {
+    isValid = isValid && (input.value.toString().trim().length > 0);
+  }
+  if (typeof input.value === 'string') {
+    if (input.maxLength != null) {
+      isValid = isValid && (input.value.length <= input.maxLength);
+    }
+    if (input.minLength != null) {
+      isValid = isValid && (input.value.length >= input.minLength);
+    }
+  }
+  if (typeof input.value === 'number') {
+    if (input.max) {
+      isValid = isValid && (input.value <= input.max);
+    }
+    if (input.min) {
+      isValid = isValid && (input.value >= input.min);
+    }
+  }
+  return isValid;
 }
 
 const input = new ProjectInput();
