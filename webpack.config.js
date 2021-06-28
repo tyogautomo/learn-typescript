e585198e8bcce1bca0ec7554d7fb582bf63e5b62
tyogautomo <yoga.utomo27@gmail.com>
2021-06-29 04:10:55 +0700

    running web through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist') //__dirname is a global constant in nodejs environment (return the project root path)
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: [/node_modules/, /basics/]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
 }
