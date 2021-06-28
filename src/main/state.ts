cafa6f0734e300e4a2b4b8268d864356f6c0656e
tyogautomo <yoga.utomo27@gmail.com>
2021-06-28 17:17:16 +0700

    move some files

---
+++ src/main/state.ts
@@ -0,0 +1,58 @@
+namespace App {
+  // Global States
+  type Listener<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listener: Listener<T>) {
+      this.listeners.push(listener);
+    }
+  }
+
+  export class ProjectState extends State<Project> {
+    private static instance: ProjectState;
+    private projects: Project[] = [];
+
+    private constructor() {
+      super();
+    } // create a singleton concept, source of truth
+
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, people: number) {
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        people,
+        ProjectStatus.Active
+      )
+
+      this.projects.push(newProject);
+      this.updateListener();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const project = this.projects.find(prj => prj.id === projectId);
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListener();
+      }
+    }
+
+    private updateListener() {
+      for (const listener of this.listeners) {
+        listener([...this.projects]);
+      }
+    }
+  }
+
+  export const projectState = ProjectState.getInstance();
+}
