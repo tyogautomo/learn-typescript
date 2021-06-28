c5250cf2058acebe9118865bfa097d2054493c50
tyogautomo <yoga.utomo27@gmail.com>
2021-06-28 17:59:24 +0700

    fixing import reference

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,9 +1,3 @@
-/// <reference path="states/projectState.ts" />
-/// <reference path="interfaces/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="helpers/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
-/// <reference path="components/baseComponents.ts" />
 /// <reference path="components/projectInput.ts" />
 /// <reference path="components/projectList.ts" />
 
